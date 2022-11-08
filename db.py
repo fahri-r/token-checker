12e1b0f891d559a04327cc364ed6f39808b94397
fahri-r <00.fahri.r@gmail.com>
2022-11-08 17:17:00 +0700

    add workflows

--- db.py
+++ db.py
@@ -1,21 +1,12 @@
 import os
-from dotenv import load_dotenv
-from pathlib import Path
-
-load_dotenv()
-env_path = Path('.')/'.env'
-load_dotenv(dotenv_path=env_path)
-
+from pymongo import MongoClient
 
 def get_database():
-    from pymongo import MongoClient
-    import pymongo
 
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
     CONNECTION_STRING = os.getenv('DB_URI')
 
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-    from pymongo import MongoClient
     client = MongoClient(CONNECTION_STRING)
 
     # Create the database for our example (we will use the same database throughout the tutorial
--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,6 @@ class TestClass:
         token_address = "0xe9e7cea3dedca5984780bafc599bd69add087d56"
         chain = "bsc"
         chat_id = -1001612967544
-
         assert add_token(token_address, chain, chat_id)
 
     def test_token_check(self):
